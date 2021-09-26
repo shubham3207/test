b828b0c735118736a1a9be051a9cb1d70481a03f
shubham3207 <shubham.sapkota1@gmail.com>
2021-09-26 18:08:27 +0545

    second commit

--- src/product.java
+++ src/product.java
@@ -6,13 +6,14 @@ import java.sql.*;
 
 public class product {
     private JPanel Main;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
+    private JTextField txtName;
+    private JTextField txtPrice;
+    private JTextField txtQty;
     private JButton saveButton1;
     private JButton deleteButton1;
     private JButton updateButton;
-    private JTextField textField4;
+    private JTextField txtpid;
+    private JButton searchButton;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("product");
@@ -24,16 +25,71 @@ public class product {
 
     public product() {
         connect();
+
         saveButton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String name,price,qty;
+
+                name = txtName.getText();
+                price = txtPrice.getText();
+                qty = txtQty.getText();
+
+                try {
+                    pst = con.prepareStatement("insert into products(pname,price,qty)values(?,?,?)");
+                    pst.setString(1, name);
+                    pst.setString(2, price);
+                    pst.setString(3, qty);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null,"Record Addedddddd!!!!");
+
+                    txtName.setText("");
+                    txtPrice.setText("");
+                    txtQty.setText("");
+                    txtName.requestFocus();
+                }
+
+                catch (SQLException e1)
+                {
+                    e1.printStackTrace();
+                }
 
             }
         });
-        saveButton1.addActionListener(new ActionListener() {
+        searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
 
+                    String pid = txtpid.getText();
+                    pst = con.prepareStatement("select pname,price,qty from products where pid = ?");
+                    pst.setString(1, pid);
+                    ResultSet rs = pst.executeQuery();
+
+                    if(rs.next()==true)
+                    {
+                        String name = rs.getString(1);
+                        String price = rs.getString(2);
+                        String qty = rs.getString(3);
+
+                        txtName.setText(name);
+                        txtPrice.setText(price);
+                        txtQty.setText(qty);
+                    }
+                    else
+                    {
+                        txtName.setText("");
+                        txtPrice.setText("");
+                        txtQty.setText("");
+                        JOptionPane.showMessageDialog(null,"Invalid Product ID");
+
+                    }
+                }
+
+                catch (SQLException ex)
+                {
+                    ex.printStackTrace();
+                }
             }
         });
     }
