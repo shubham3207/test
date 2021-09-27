1741a933f071d681b5e462275de3b24ff607e272
shubham3207 <shubham.sapkota1@gmail.com>
2021-09-27 23:41:15 +0545

    product class

--- src/product.java
+++ src/product.java
@@ -4,3 +4,14 @@ import java.awt.event.ActionListener;
 import java.sql.DriverManager;
 import java.sql.*;
 
+public class product {
+    private JPanel Main;
+    private JTextField txtName;
+    private JTextField txtPrice;
+    private JTextField txtQty;
+    private JButton saveButton1;
+    private JButton deleteButton1;
+    private JButton updateButton;
+    private JTextField txtpid;
+    private JButton searchButton;
+
