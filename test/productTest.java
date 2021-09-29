ece98f740bbf92994069ed13aaad9e9ff2a49475
shubham3207 <shubham.sapkota1@gmail.com>
2021-09-29 10:02:42 +0545

    unit testing

---
+++ test/productTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class productTest {
+
+    @Test
+    public void main() {
+        String name;
+        name = "samsung";
+        assertEquals("samsung",name);
+    }
+}
